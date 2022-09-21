68f3e6a9c594a59d300f837635e80a16518ad4d7
TiborNorbert <112852931+TiborNorbert@users.noreply.github.com>
2022-09-21 13:08:24 +0200

    test 2.0

--- test 1.0/Program.cs
+++ test 1.0/Program.cs
@@ -13,14 +13,7 @@ namespace test_1._0
             Console.WriteLine("Adjon be 2-t számot te");
             int szam1 = Convert.ToInt32(Console.ReadLine());
             int szam2 = Convert.ToInt32(Console.ReadLine());
-            if (szam1>szam2)
-            {
-                Console.WriteLine("Az egyes szám nagyobb");
-            }
-            else
-            {
-                Console.WriteLine("Az kettes szám a nagyobb");
-            }
+
             Console.ReadKey();
         }
     }
